26c00f56a61810036b4f078549aca4348abd7d0b
Rzkrayann <rayan.el-armouzi@etu.univ-cotedazur.fr>
2024-12-05 22:39:08 +0100

    add beach component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BeachComponent } from './beach/beach.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BeachComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'clean-the-ocean';
